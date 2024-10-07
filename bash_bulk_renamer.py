1691e041d0faaba34f127e563234950ee1b2ba30
jaxios <giuseppe@nb-inservice-03.INSERVICE.LOCALE>
2024-10-07 17:43:39 +0200

    Added check for missing EXIF data

--- bash_bulk_renamer.py
+++ bash_bulk_renamer.py
@@ -1,15 +1,14 @@
 import os
 import shutil
 import sys
-import pathlib
 import mediaExifExtractor
 
-def printHelp():
+def print_help():
     syntax = open("README.md", "r")
     print(syntax.read())
 
 
-def sanitizeFolderName(folder):
+def sanitize_folder_name(folder):
     folder = folder.split('/')
     
     if folder[-1] == "":
@@ -17,17 +16,17 @@ def sanitizeFolderName(folder):
     else:
         return folder[-1].split(' ', 1)[1]
 
-def getExifData(file, data):
+def get_exif_data(file, data):
 
     #extract EXIF information from file
 
-    result = mediaExifExtractor.getExifData(file, data)
+    result = mediaExifExtractor.get_exif_data(file, data)
 
     return result
 
 
 
-def renameFile(path, target_file, renamer_template, folder):
+def rename_file(path, target_file, renamer_template, folder):
 
     file_extension = os.path.splitext(target_file)[1]
 
@@ -36,8 +35,8 @@ def renameFile(path, target_file, renamer_template, folder):
     renamer_template = renamer_template.replace("%", "")
     renamer_template = renamer_template.replace("]", "")
     renamer_template = renamer_template.replace("$folder", folder)
-    renamer_template = renamer_template.replace("$datetime", getExifData(path+target_file, "datetime"))
-    renamer_template = renamer_template.replace("$device", getExifData(path+target_file, "device"))
+    renamer_template = renamer_template.replace("$datetime", get_exif_data(path+target_file, "datetime"))
+    renamer_template = renamer_template.replace("$device", get_exif_data(path+target_file, "device"))
 
     #convert extension to lower case
     new_file_name = renamer_template + file_extension.lower()
@@ -52,18 +51,19 @@ IN_PLACE=False
 #Check arguments and syntax
 
 if "--help" in sys.argv:
-    printHelp()
+    print_help()
     sys.exit(0)
 
 print("==================================================")
+
 if len(sys.argv) < 3:
-    printHelp()
+    print_help()
     sys.exit(1)
 
 if not os.path.exists(sys.argv[1]):
     print("Non-existent target folder [" + sys.argv[1] + "]")
     print("==================================================")
-    printHelp()
+    print_help()
     sys.exit(1)
 
 target_path = sys.argv[1]
@@ -71,7 +71,7 @@ target_path = sys.argv[1]
 if not os.path.isfile("./templates/"+sys.argv[2]):
     print("Non-existent template")
     print("==================================================")
-    printHelp()
+    print_help()
     sys.exit(1)
 
 if "--dry-run" in sys.argv:
@@ -86,7 +86,7 @@ print("In place: "+str(IN_PLACE))
 print("==================================================")
 
 #Sanitize folder name
-folder_name = sanitizeFolderName(target_path)
+folder_name = sanitize_folder_name(target_path)
 #complete_path = str(pathlib.Path(__file__).parent.resolve()) + target_path.replace(".", "")
 complete_path = target_path
 #create log and write parameters
@@ -117,7 +117,7 @@ for f in os.listdir(target_path):
 #sort files by EXIF datetime
 sorted_files = []
 for target_file in original_files:
-    sorted_files.append([target_file, getExifData(complete_path+target_file,"datetime")])
+    sorted_files.append([target_file, get_exif_data(complete_path+target_file,"datetime")])
 
 sorted_files = sorted(sorted_files, key=lambda x:x[1])
 
@@ -128,12 +128,22 @@ if not DRY_RUN and not os.path.exists(target_path + "backup/"):
 
 
 i = 1
+IGNORE_MISSING_DATA = False
+
 for target_file in sorted_files:
-    new_name = "[" + str(i).rjust(index_digits, "0") + "]" + renameFile(complete_path,target_file[0], renamer_template,folder_name)
+    new_name = "[" + str(i).rjust(index_digits, "0") + "]" + rename_file(complete_path,target_file[0], renamer_template,folder_name)
     print(target_file[0] + " > " + new_name)
     log_file.write(target_file[0] + " > " + new_name + "\n")
 
     if not DRY_RUN:
+
+        if "No EXIF data" in new_name and IGNORE_MISSING_DATA is False:
+            no_data_check = input("Not all files have complete EXIF data, continue anyway? (y/n)")
+            if "n" in no_data_check:
+                sys.exit(1)
+            else:
+                IGNORE_MISSING_DATA = True
+
         if not IN_PLACE:
             shutil.copy2(target_path + target_file[0], target_path + "backup/" + target_file[0])
         shutil.move(target_path + target_file[0], target_path + new_name)
