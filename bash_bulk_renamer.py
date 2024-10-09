9000480817338eb15560db984c204b3f23d8f662
jaxios <giuseppe@nb-inservice-03.INSERVICE.LOCALE>
2024-10-09 17:37:27 +0200

    Working on automated restores

--- bash_bulk_renamer.py
+++ bash_bulk_renamer.py
@@ -1,7 +1,9 @@
 import os
 import shutil
 import sys
+from datetime import datetime
 import mediaExifExtractor
+import json
 
 def print_help():
     syntax = open("README.md", "r")
@@ -24,8 +26,6 @@ def get_exif_data(file, data):
 
     return result
 
-
-
 def rename_file(path, target_file, renamer_template, folder):
 
     file_extension = os.path.splitext(target_file)[1]
@@ -45,8 +45,10 @@ def rename_file(path, target_file, renamer_template, folder):
 
 #==================================================================================================#
 
-DRY_RUN=False
-IN_PLACE=False
+DRY_RUN = False
+IN_PLACE = False
+RECURSIVE = False
+RESTORE = False
 
 #Check arguments and syntax
 
@@ -67,88 +69,145 @@ if not os.path.exists(sys.argv[1]):
     sys.exit(1)
 
 target_path = sys.argv[1]
+if target_path[-1] != "/": #add final slash if missing
+    target_path = target_path + "/"
 
-if not os.path.isfile("./templates/"+sys.argv[2]):
-    print("Non-existent template")
-    print("==================================================")
-    print_help()
-    sys.exit(1)
+if "--restore" in sys.argv:
+    RESTORE = True
+else:
+    if not os.path.isfile("./templates/"+sys.argv[2]):
+        print("Non-existent template")
+        print("==================================================")
+        print_help()
+        sys.exit(1)
+
+if "--y" in sys.argv:
+    IGNORE_MISSING_DATA = True
 
 if "--dry-run" in sys.argv:
-    DRY_RUN=True
+    DRY_RUN = True
 if "--in-place" in sys.argv:
-    IN_PLACE=True
+    IN_PLACE = True
+if "--recursive" in sys.argv:
+    RECURSIVE = True
 
-print(target_path)
+print("Target path: " + target_path)
 print("Dry run: "+str(DRY_RUN))
 print("In place: "+str(IN_PLACE))
+print("Recursive: "+str(RECURSIVE))
 
 print("==================================================")
 
-#Sanitize folder name
-folder_name = sanitize_folder_name(target_path)
-#complete_path = str(pathlib.Path(__file__).parent.resolve()) + target_path.replace(".", "")
-complete_path = target_path
-#create log and write parameters
 
-log_file = open(complete_path + "log.txt", "w")
-log_file.write("Target: "+ target_path + "\n")
-log_file.write("Dry run: "+str(DRY_RUN) + "\n")
-log_file.write("In place: "+str(IN_PLACE) + "\n\n")
+if RESTORE:
+    log_list = []
+    for f in os.listdir(target_path):
+        ext = os.path.splitext(f)[1]
+        if (os.path.isfile(target_path + f) and ext.lower() == ".json"):
+            log_list.append(f)
+    
+    if len(log_list) == 1:
+        target_log = log_list[0]
+    else:
+        i = 0
+        for log in log_list:
+            print("%d: %s" % (i,log))
+            i+=1
+        target_log = log_list[int(input("Which version to restore?: "))]
+    
+    with open(target_path + target_log, 'r') as openfile:
+        # Reading from json file
+        backup = json.load(openfile)
+ 
+    for old, new in backup.items():
+        if old == "Datetime":
+            continue
 
-
-
-#Read and interpret template
-
-renamer_template = open("./templates/"+sys.argv[2], "r")
-renamer_template = renamer_template.read()
-index_digits = renamer_template.count("%")
-
-
-#cicle trough files and imports only certain formats
-
-valid_formats = [".jpg",".mp4",".jpeg"]
-original_files = []
-for f in os.listdir(target_path):
-    ext = os.path.splitext(f)[1]
-    if (os.path.isfile(target_path + f) and ext.lower() in valid_formats):
-        original_files.append(f)
-
-#sort files by EXIF datetime
-sorted_files = []
-for target_file in original_files:
-    sorted_files.append([target_file, get_exif_data(complete_path+target_file,"datetime")])
-
-sorted_files = sorted(sorted_files, key=lambda x:x[1])
-
-#rename files with log
-
-if not DRY_RUN and not os.path.exists(target_path + "backup/"):
-    os.makedirs(target_path + "backup/")
+        print(new + " > " + old)
+        if not DRY_RUN:
+            shutil.move(target_path + new, target_path + old)
+    
+    exit(0)
 
 
-i = 1
-IGNORE_MISSING_DATA = False
+folders_list = []
+if RECURSIVE:
+    for subfolder in next(os.walk(target_path))[1]:
+        folders_list.append(target_path + subfolder + "/")
+else:
+    folders_list.append(target_path)
+    if len(next(os.walk(target_path))[1]) > 1:
+        recursive_check = input("Subfolders detected, but no --recursive option used, are you sure to continue? (y/n) ")
+        if "y" not in recursive_check:
+            sys.exit(1)
 
-for target_file in sorted_files:
-    new_name = "[" + str(i).rjust(index_digits, "0") + "]" + rename_file(complete_path,target_file[0], renamer_template,folder_name)
-    print(target_file[0] + " > " + new_name)
-    log_file.write(target_file[0] + " > " + new_name + "\n")
+for folder in folders_list:
+    print("==================================================\n" + folder)
 
-    if not DRY_RUN:
+    #Sanitize folder name
+    folder_name = sanitize_folder_name(folder) #returns folder name without leading index ([###])
+    complete_path = folder
 
-        if "No EXIF data" in new_name and IGNORE_MISSING_DATA is False:
-            no_data_check = input("Not all files have complete EXIF data, continue anyway? (y/n)")
-            if "n" in no_data_check:
-                sys.exit(1)
-            else:
-                IGNORE_MISSING_DATA = True
+    #Read and interpret template
 
-        if not IN_PLACE:
-            shutil.copy2(target_path + target_file[0], target_path + "backup/" + target_file[0])
-        shutil.move(target_path + target_file[0], target_path + new_name)
+    renamer_template = open("./templates/"+sys.argv[2], "r")
+    renamer_template = renamer_template.read()
+    index_digits = renamer_template.count("%")
+
+    #cicle trough files and imports only certain formats
+
+    valid_formats = [".jpg",".mp4",".jpeg"]
+    original_files = []
+    for f in os.listdir(folder):
+        ext = os.path.splitext(f)[1]
+        if (os.path.isfile(folder + f) and ext.lower() in valid_formats):
+            original_files.append(f)
+
+    #sort files by EXIF datetime
+    sorted_files = []
+    for target_file in original_files:
+        sorted_files.append([target_file, get_exif_data(complete_path+target_file,"datetime")])
+
+    sorted_files = sorted(sorted_files, key=lambda x:x[1])
+
+    #rename files with log
+
+    timestamp = str(datetime.now().strftime("%Y-%m-%d_%H-%M-%S"))
+
+    if not DRY_RUN and not os.path.exists(folder + "backup_ " + timestamp +"/"):
+        os.makedirs(folder + "backup_ " + timestamp +"/")
 
 
-    i+=1
+    i = 1
+    filename_changes = 0
 
-log_file.close()
+    changelog = {} #store old and new name for logging and restore
+    changelog["Datetime"] = timestamp
+
+    for target_file in sorted_files:
+        new_name = "[" + str(i).rjust(index_digits, "0") + "]" + rename_file(complete_path,target_file[0], renamer_template,folder_name)
+        print("    |_ " + target_file[0] + " > " + new_name)
+        changelog[target_file[0]] = new_name
+
+        if target_file[0] != new_name:
+            filename_changes += 1
+
+        if not DRY_RUN:
+
+            if "No EXIF data" in new_name and IGNORE_MISSING_DATA is False:
+                no_data_check = input("Not all files have complete EXIF data, continue anyway? (y/n) ")
+                if "n" in no_data_check:
+                    sys.exit(1)
+                else:
+                    IGNORE_MISSING_DATA = True
+
+            if not IN_PLACE:
+                shutil.copy2(folder + target_file[0], folder + "backup_ " + timestamp +"/" + target_file[0])
+            shutil.move(folder + target_file[0], folder + new_name)
+
+
+        i+=1
+
+    if filename_changes > 0:
+        with open(complete_path + timestamp + ".json", "w") as outfile: #write log to json
+            outfile.write(json.dumps(changelog, indent=4))
